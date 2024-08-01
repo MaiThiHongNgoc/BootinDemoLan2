09f98199580eaf84585594949060a0cac97e4600
The-Quan <145833348+The-Quan@users.noreply.github.com>
2024-08-01 14:32:21 +0700

    up

--- src/Backend/Order/orderList.js
+++ src/Backend/Order/orderList.js
@@ -67,13 +67,13 @@ const OrderList = () => {
 
     const handleStatusChange = async (e, order) => {
         const updatedStatus = e.target.value;
-    
+
         if (order.status === 'CANCELLED') {
             alert('Cannot change the status of a cancelled order.');
             return;
         }
 
-          if (order.status === 'COMPLETED') {
+        if (order.status === 'COMPLETED') {
             alert('Cannot change the status of a cancelled order.');
             return;
         }
@@ -84,7 +84,7 @@ const OrderList = () => {
         }
 
         const updatedOrder = { ...order, status: updatedStatus };
-    
+
         const payload = {
             user: { user_id: updatedOrder.user.user_id },
             address: updatedOrder.address,
@@ -92,7 +92,7 @@ const OrderList = () => {
             total_amount: updatedOrder.total_amount,
             status: updatedStatus
         };
-    
+
         setLoading(true);
         try {
             await updateOrder(updatedOrder.order_id, payload);
