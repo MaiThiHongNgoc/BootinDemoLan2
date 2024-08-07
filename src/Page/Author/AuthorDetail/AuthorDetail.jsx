97afea1216f80e1cffcecbc0e0968830a10728a6
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-08-07 15:45:57 +0700

    .

--- src/Page/Author/AuthorDetail/AuthorDetail.jsx
+++ src/Page/Author/AuthorDetail/AuthorDetail.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { RxSlash } from "react-icons/rx";
+import { TiSocialFacebook } from "react-icons/ti";
+import { TiSocialGooglePlus } from "react-icons/ti";
+import { TiSocialTwitter } from "react-icons/ti";
+import Header from '../../../Component/Header/Header';
+import Footer from '../../../Component/Footer/Footer';
+import './AuthorDetail.css';
 import { getAuthorById } from '../../../Backend/Service (1)/authorService';
 
 const AuthorDetail = () => {
@@ -24,11 +31,55 @@ const AuthorDetail = () => {
 
   return (
     <div>
+      <Header/>
+    <div className='detail-title'>
+      <div className='detail-jpg'>
+        <h1 className='detail-new'>Dang Diep</h1>
+        <div className='detail-breadcrumb'>
+          <div className='detail-bwp'>
+            <a className='detail-a' href='http://localhost:3000/'>Home</a>
+            <span className='detail-delimiter'>
+              <i className='detail-slash'><RxSlash /></i>
+              Author
+            </span>
+            <span className='detail-delimiter'>
+              <i className='detail-slash'><RxSlash /></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='detail-container'>
+      <div className='detail-author'>
+        <div className='detail-url'>
+          <img className='detail-img' src={author.url_img} alt={author.author_name} />
+        </div>
+      <div className='detail-info'>
       <h1>{author.author_name}</h1>
-      <img src={author.url_img} alt={author.author_name} />
-      <p>Published Books: {author.published_book}</p>
       <p>Bio: {author.bio}</p>
+      <p>Published Books: {author.published_book}</p>
       {/* Hiển thị thêm thông tin nếu cần */}
+      <ul className='detail-ul'>
+        <li>
+          <a href='#'>
+            <i className='detail-social'><TiSocialFacebook /></i>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <i className='detail-social'><TiSocialTwitter /></i>
+          </a>
+        </li>
+        <li>
+          <a href='#'>
+            <i className='detail-social'><TiSocialGooglePlus /></i>
+          </a>
+        </li>
+      </ul>
+      </div>
+      </div>
+    </div>
+    <Footer/>
     </div>
   );
 };
